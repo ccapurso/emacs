d9af6bde31a40a6d8c628c39a9036c413ab6de9f
Chris Capurso <chrisc@stitchdata.com>
2018-06-12 09:10:12 -0400

    set js-indent-level

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -124,6 +124,7 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+ '(js-indent-level 2)
  '(package-selected-packages
    (quote
     (rainbow-delimiters yasnippet yaml-mode terraform-mode smex projectile php-mode paredit org mediawiki markdown-mode magit ido-vertical-mode ido-ubiquitous fixme-mode expand-region company coffee-mode cider better-defaults bats-mode align-cljlet ag)))
